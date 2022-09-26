ca622013ed85f4434a631e4148d27d6d9ffa4c87
vanesa mariel herrera <vanesaailin@hotmail.com>
2022-09-26 20:10:07 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio 75 lista/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercico75
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> listastring = new List<string>();
+
+
+            Console.WriteLine("¿hola cual es tu nombre");
+            string nombre = Console.ReadLine();//puedo usar mucho texto
+            Console.WriteLine("¿" + nombre + "ingresa tu apellido?");
+            string cadena = Console.ReadLine();
+            Console.WriteLine("¿ cual es tu calle de donde vivis");
+            string direccion = Console.ReadLine();
+
+            listastring.Add(nombre);
+            listastring.Add(cadena);
+            listastring.Add(direccion);
+
+            for (int i = 0; i < listastring.Count; i++)
+            {
+                string dato = listastring[i];
+                Console.WriteLine(dato);
+            }
+        }
+    }
+}
